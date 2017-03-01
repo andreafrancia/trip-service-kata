5c693d63f7ce558cea15c105f59b065730a70954
Andrea Francia <andrea@andreafrancia.it>
2017-03-01 18:48:18 +0100

    Aggiunti test

--- php/test/Test/TripServiceKata/Trip/TripServiceTest.php
+++ php/test/Test/TripServiceKata/Trip/TripServiceTest.php
@@ -3,10 +3,45 @@
 namespace Test\TripServiceKata\Trip;
 
 use PHPUnit_Framework_TestCase;
+use TripServiceKata\Exception\DependentClassCalledDuringUnitTestException;
+use TripServiceKata\Exception\UserNotLoggedInException;
+use TripServiceKata\Trip\Trip;
+use TripServiceKata\Trip\TripService;
+use TripServiceKata\User\User;
 
 class TripServiceTest extends PHPUnit_Framework_TestCase
 {
-    public function test_nothing() {
+    public function setUp()
+    {
+        $this->service = new TestableTripService();
+    }
 
+    public function test_when_user_is_not_logged_in_it_will_throw_an_exception() {
+
+        $this->expectException(UserNotLoggedInException::class);
+        $this->service->getTripsByUser(new User(null));
+    }
+
+    public function test_when_user_is_logged_in_but_is_not_a_friend_the_list_is_empty() {
+
+        $this->service->setLoggedInUser(new User(null));
+        $trips = $this->service->getTripsByUser(new User(null));
+
+        self::assertEquals(array(), $trips);
+    }
+}
+
+class TestableTripService extends TripService
+{
+    private $loggedInUser;
+
+    public function getLoggedInUser()
+    {
+        return $this->loggedInUser;
+    }
+
+    public function setLoggedInUser($param)
+    {
+        $this->loggedInUser = $param;
     }
 }
