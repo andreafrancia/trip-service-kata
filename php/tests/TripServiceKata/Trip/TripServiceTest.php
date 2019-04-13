d50a4cec4c66771307f7ff2e9d7644b40f84fd44
Andrea Francia <andrea@andreafrancia.it>
2019-04-13 16:51:43 +0200

    Pass.

--- php/tests/TripServiceKata/Trip/TripServiceTest.php
+++ php/tests/TripServiceKata/Trip/TripServiceTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class TripServiceTest extends TestCase
 {
     function test_something() {
-        // 
+        new TripService();
         self::assertEquals(1,1);
     }
 }
