574aee9c2578e7a915f9119f1dded732a7af03af
Andrea Francia <andrea@andreafrancia.it>
2019-04-13 16:54:56 +0200

    Pass.

--- php/tests/TripServiceKata/Trip/TripServiceTest.php
+++ php/tests/TripServiceKata/Trip/TripServiceTest.php
@@ -3,11 +3,24 @@
 namespace TripServiceKata\Trip;
 
 use PHPUnit\Framework\TestCase;
+use TripServiceKata\Exception\DependentClassCalledDuringUnitTestException;
+use TripServiceKata\User\User;
 
 class TripServiceTest extends TestCase
 {
-    function test_something() {
-        new TripService();
-        self::assertEquals(1,1);
+    /** @var TripService */
+    private $service;
+
+    function test_something()
+    {
+        $this->expectException(DependentClassCalledDuringUnitTestException::class);
+        
+        $this->service->getTripsByUser(new User(""));
+    }
+
+    protected function setUp(): void
+    {
+        $this->service = new TripService();
+        parent::setUp();
     }
 }
